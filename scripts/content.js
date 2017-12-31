e0c24123da3212385728aefe89f76c93e6ec3cd6
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-12-31 16:41:04 -0500

    Create switch statement for different profiles

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,20 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("something happening from the extension");
   var data = request.data || {};
 
-  if (data[3] == "Twitter"){
-    document.getElementById("full-name").value = data[0]
-    document.getElementById("email").value = data[1];
-    document.getElementById("password").value = data[2];
-  };
-//  else if (data[3] !== "Twitter") {alert("hmm")}
+  switch (true) {
+    case data[3] == "Twitter":
+    document.getElementById("full-name").value = data[0] + " " + data[1];
+    document.getElementById("email").value = data[2];
+    document.getElementById("password").value = data[3];
+    break;
+
+    case data[3] == "LinkedIn":
+    document.getElementById("reg-firstname").value = data[0];
+    document.getElementById("reg-lastname").value = data[1];
+    document.getElementById("reg-email").value = data[2];
+    document.getElementById("reg-password").value = data[3];
+    break;
+  }
 
   //sendResponse({url: url, success: true});
 });
