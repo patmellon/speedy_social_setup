3829b7c71ac5db6903bc8006b4138295cbe16003
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-12-31 12:39:26 -0500

    Replace jquery with vanilla js

--- scripts/extension.js
+++ scripts/extension.js
@@ -12,16 +12,20 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("status").textContent = "Example: clientname.com";
   var button = document.getElementById("changelinks");
   button.addEventListener("click", function() {
-    $("#status").html("Clicked change links button");
+    document.getElementById("status").textContent = "Clicked button!";
 
-    var name = [$("#name").val(), $("#email").val(), $("#password").val()];
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-    if (!name) {
-      $("#status").html("Invalid text provided");
+    var data = [name, email, password];
+
+    if (!data) {
+      document.getElementById("status").textContent = "Invalid text provided";
       return;
     }
 
-    chrome.storage.sync.set({ data: name }, function() {
+    chrome.storage.sync.set({ data: data }, function() {
       if (chrome.runtime.error) {
         console.log("Runtime error.");
       }
@@ -29,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { url: url }, function(response) {
-        $("#status").html("Form updated!");
+        document.getElementById("status").textContent = "Form updated!";
         console.log("success");
       });
     });
