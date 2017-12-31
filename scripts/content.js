6e10fa7e08f0e1e6e87bc898439276398468f7c2
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-12-31 15:42:19 -0500

    Update Twitter form

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("something happening from the extension");
-  var url = request.url || {};
-  var url_dash = request.url.replace(/[.]/i, "-");
-  var upload_url_path = "http://" + url + "/wp-content/uploads/" + url_dash;
+  var data = request.data || {};
 
-  document.getElementById("siteurl").value = "http://" + url;
-  document.getElementById("home").value = "http://" + url;
-  document.getElementById("permalink_structure").value = "/%postname%/";
-  document.getElementById("uploads_use_yearmonth_folders").value = 0;
-  document.getElementById("upload_path").value =
-    "wp-content/uploads/" + url_dash;
-  document.getElementById("upload_url_path").value = upload_url_path;
+  if (data[3] == "Twitter"){
+    document.getElementById("full-name").value = data[0]
+    document.getElementById("email").value = data[1];
+    document.getElementById("password").value = data[2];
+  };
+//  else if (data[3] !== "Twitter") {alert("hmm")}
 
   //sendResponse({url: url, success: true});
 });
