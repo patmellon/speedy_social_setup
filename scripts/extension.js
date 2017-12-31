96a3c4364c8b2b7425161b8d76444fba375b7b13
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-12-31 15:42:53 -0500

    Display link based on which profile is selected

--- scripts/extension.js
+++ scripts/extension.js
@@ -1,15 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  //Load saved input
+
   document.body.onload = function() {
     chrome.storage.sync.get("data", function(items) {
       if (!chrome.runtime.error) {
         document.getElementById("name").value = items.data[0];
         document.getElementById("email").value = items.data[1];
-        document.getElementById("password").value = items.data[2];
       }
     });
   };
 
-  document.getElementById("status").textContent = "Example: clientname.com";
+  //Show link based on user selection
+  var profileLink = ""
+  var form = document.getElementById("profiles");
+  form.addEventListener("change", function() {
+    var profileValue = document.getElementById("profiles").value
+    if (profileValue == "Twitter") {
+      document.getElementById("link").innerHTML = "<a href=''>Visit " + profileValue + "</a>";
+      profileLink = "http://www.twitter.com/signup"
+    }
+  })
+
+  //Open link on click
+
+  var link = document.getElementById("link")
+  link.addEventListener("click", function(){
+    chrome.tabs.create({ url: profileLink })
+  })
+
   var button = document.getElementById("changelinks");
   button.addEventListener("click", function() {
     document.getElementById("status").textContent = "Clicked button!";
@@ -17,22 +36,31 @@ document.addEventListener("DOMContentLoaded", function() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var profile = document.getElementById("profiles");
+    var selectedProfile = profile.options[profile.selectedIndex].value;
 
-    var data = [name, email, password];
+    var data = [name, email, password, selectedProfile];
+    var savedData = [name, email];
 
-    chrome.storage.sync.set({ data: data }, function() {
+    //Store saved data (only username and email)
+
+    chrome.storage.sync.set({ data: savedData }, function() {
       if (chrome.runtime.error) {
         console.log("Runtime error.");
       }
     });
 
+    //Send the data to content.js to update the website form
+
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { url: url }, function(response) {
+      chrome.tabs.sendMessage(tabs[0].id, { data: data }, function(response) {
         document.getElementById("status").textContent = "Form updated!";
         console.log("success");
       });
     });
   });
+
+  //Clear data from from and storage
 
   var clear = document.getElementById("clear");
   clear.addEventListener("click", function() {
