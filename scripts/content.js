82d47db0e4e2616ce07cce619576b44707e2cc94
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-02 17:54:57 -0500

    Change data array to object

--- scripts/content.js
+++ scripts/content.js
@@ -4,46 +4,46 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
   switch (true) {
 
-    case data[3] == "Twitter":
-    document.getElementById("full-name").value = data[0] + " " + data[1];
-    document.getElementById("email").value = data[2];
+    case data.selectedProfile == "Twitter":
+    document.getElementById("full-name").value = data.firstName + " " + data.lastName;
+    document.getElementById("email").value = data.email;
     break;
 
-    case data[3] == "LinkedIn":
-    document.getElementById("reg-firstname").value = data[0];
-    document.getElementById("reg-lastname").value = data[1];
-    document.getElementById("reg-email").value = data[2];
+    case data.selectedProfile == "LinkedIn":
+    document.getElementById("reg-firstname").value = data.firstName;
+    document.getElementById("reg-lastname").value = data.lastName;
+    document.getElementById("reg-email").value = data.email;
     break;
 
-    case data[3] == "Slideshare":
-    document.getElementById("j-user-login").value = data[0] + data[1];
-    document.getElementById("j-user-email").value = data[2];
+    case data.selectedProfile == "Slideshare":
+    document.getElementById("j-user-login").value = data.firstName + data.lastName;
+    document.getElementById("j-user-email").value = data.email;
     break;
 
-    case data[3] == "Levo":
-    document.getElementById("first_name").value = data[0];
-    document.getElementById("last_name").value = data[1];
-    document.getElementById("email").value = data[2];
+    case data.selectedProfile == "Levo":
+    document.getElementById("first_name").value = data.firstName;
+    document.getElementById("last_name").value = data.lastName;
+    document.getElementById("email").value = data.email;
     break;
 
-    case data[3] == "Behance":
-    document.getElementById("first_name").value = data[0];
-    document.getElementById("last_name").value = data[1];
-    document.getElementById("email_address").value = data[2];
+    case data.selectedProfile == "Behance":
+    document.getElementById("first_name").value = data.firstName;
+    document.getElementById("last_name").value = data.lastName;
+    document.getElementById("email_address").value = data.email;
     break;
 
-    case data[3] == "Tumblr":
-    document.getElementById("signup_username").value = data[0] + data[1];
-    document.getElementById("signup_email").value = data[2];
+    case data.selectedProfile == "Tumblr":
+    document.getElementById("signup_username").value = data.firstName + data.lastName;
+    document.getElementById("signup_email").value = data.email;
     break;
 
-    case data[3] == "Pinterest":
-    document.getElementById("email").value = data[2];
+    case data.selectedProfile == "Pinterest":
+    document.getElementById("email").value = data.email;
     break;
 
-    case data[3] == "Wordpress":
-    document.getElementById("username").value = data[0] + data[1];
-    document.getElementById("email").value = data[2];
+    case data.selectedProfile == "Wordpress":
+    document.getElementById("username").value = data.firstName + data.lastName;
+    document.getElementById("email").value = data.email;
     break;
 
   }
