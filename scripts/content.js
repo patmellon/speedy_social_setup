3a509cc559c9cfbcffe5a4c237263bdf280a2152
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-02 19:08:03 -0500

    Add vimeo link and styling

--- scripts/content.js
+++ scripts/content.js
@@ -3,49 +3,57 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var data = request.data || {};
 
   switch (true) {
-
     case data.selectedProfile == "Twitter":
-    document.getElementById("full-name").value = data.firstName + " " + data.lastName;
-    document.getElementById("email").value = data.email;
-    break;
+      document.getElementById("full-name").value =
+        data.firstName + " " + data.lastName;
+      document.getElementById("email").value = data.email;
+      break;
 
     case data.selectedProfile == "LinkedIn":
-    document.getElementById("reg-firstname").value = data.firstName;
-    document.getElementById("reg-lastname").value = data.lastName;
-    document.getElementById("reg-email").value = data.email;
-    break;
+      document.getElementById("reg-firstname").value = data.firstName;
+      document.getElementById("reg-lastname").value = data.lastName;
+      document.getElementById("reg-email").value = data.email;
+      break;
 
     case data.selectedProfile == "Slideshare":
-    document.getElementById("j-user-login").value = data.firstName + data.lastName;
-    document.getElementById("j-user-email").value = data.email;
-    break;
+      document.getElementById("j-user-login").value =
+        data.firstName + data.lastName;
+      document.getElementById("j-user-email").value = data.email;
+      break;
 
     case data.selectedProfile == "Levo":
-    document.getElementById("first_name").value = data.firstName;
-    document.getElementById("last_name").value = data.lastName;
-    document.getElementById("email").value = data.email;
-    break;
+      document.getElementById("first_name").value = data.firstName;
+      document.getElementById("last_name").value = data.lastName;
+      document.getElementById("email").value = data.email;
+      break;
 
     case data.selectedProfile == "Behance":
-    document.getElementById("first_name").value = data.firstName;
-    document.getElementById("last_name").value = data.lastName;
-    document.getElementById("email_address").value = data.email;
-    break;
+      document.getElementById("first_name").value = data.firstName;
+      document.getElementById("last_name").value = data.lastName;
+      document.getElementById("email_address").value = data.email;
+      break;
 
     case data.selectedProfile == "Tumblr":
-    document.getElementById("signup_username").value = data.firstName + data.lastName;
-    document.getElementById("signup_email").value = data.email;
-    break;
+      document.getElementById("signup_username").value =
+        data.firstName + data.lastName;
+      document.getElementById("signup_email").value = data.email;
+      break;
 
     case data.selectedProfile == "Pinterest":
-    document.getElementById("email").value = data.email;
-    break;
+      document.getElementById("email").value = data.email;
+      break;
 
     case data.selectedProfile == "Wordpress":
-    document.getElementById("username").value = data.firstName + data.lastName;
-    document.getElementById("email").value = data.email;
-    break;
+      document.getElementById("username").value =
+        data.firstName + data.lastName;
+      document.getElementById("email").value = data.email;
+      break;
 
+    case data.selectedProfile == "Vimeo":
+      document.getElementById("signup_name").value =
+        data.firstName + " " + data.lastName;
+      document.getElementById("signup_email").value = data.email;
+      break;
   }
 
   //sendResponse({url: url, success: true});
